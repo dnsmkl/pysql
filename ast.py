50627738a8114e5d1157d0ef448994b6ae840496
Danas Mikelinskas <danas.mikelinskas@gmail.com>
2014-11-29 20:24:45 +0200

    Formmatting + remove garbage

--- ast.py
+++ ast.py
@@ -1,12 +1,8 @@
-
-
-
-
 class Node(object):
     children = None
+
     # def __init__(self)
     #     self.children = None
-
 
     def __str__(self):
         """Subclasses have to implement name attribute"""
@@ -16,17 +12,14 @@ class Node(object):
 class Statement(Node):
     name = "Statement"
 
+
 class ClauseSelect(Node):
     name = "Select"
+
 
 class ClauseFrom(Node):
     name = "From"
 
+
 class ClauseWhere(Node):
     name = "Where"
-
-
-
-
-
-print y
